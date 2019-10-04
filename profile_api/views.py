2410f4d3924524e5cd304ed1fc627626e3251eb6
essam <essam.hazem03@gmail.com>
2019-10-04 16:08:38 +0200

    added viewsets to api

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status ,viewsets
+
 from . import serializers
 
 
@@ -21,6 +22,9 @@ class HelloApiView(APIView):
 
         return Response({'message': 'Hello!', 'an_apiview': self.an_apiview})
 
+    def get(self,request,pk=None):
+
+        return Response({'message': 'Hello!'})
 
     def post (self,request):
         serializer = self.serializer_class(data=request.data)
@@ -48,3 +52,49 @@ class HelloApiView(APIView):
         """Delete an object"""
 
         return Response({'method': 'DELETE'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer ; 
+
+    def list(self,request):
+
+        a_viewset = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+        return Response({'message': 'Hello!', 'list': a_viewset})
+
+    def create(self,request):
+        
+        serializer = self.serializer_class(data= request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'hello ' + name 
+            return Response({'message',message})
+
+        else :
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request,pk=None):
+        return Response({"message","get"})
+
+
+    def update(self,request,pk =None):
+        return Response({"message","put"})
+
+
+    def partial_update(self,request,pk=None):
+        return Response({'message','update'})
+
+
+    def destroy(self,request,pk = None):
+        return Response({'message','Delete'})
+
+
+
+
